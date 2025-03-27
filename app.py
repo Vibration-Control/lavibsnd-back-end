cc6ea9f6e1f5e114bb729229ae8f6abc0fc898e1
EduardoSalmoria <eduardosalmoria@gmail.com>
2025-03-26 21:18:39 -0500

    Adding code to avoid CORS issue in the front-end

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from neutralizer_optimization import neutralizer_optimization
 
-
 app = Flask(__name__)
+# Allow all origins
+CORS(app, supports_credentials=True)
 
 @app.route('/optimizeNeutralizer', methods=['POST'])
 def optimizeNeutralizer():
     json_payload = request.json
     result = neutralizer_optimization(json_payload)
-    return result
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
