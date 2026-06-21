973f195f08d9ed4738c4bd8267a13b6f316d1a1c
EduardoSalmoria <eduardosalmoria@gmail.com>
2026-06-21 17:39:33 -0500

    Adjusting objective function to consider different objectives

--- services/objective_function_service.py
+++ services/objective_function_service.py
@@ -1,84 +1,85 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def objective_function(optimization_data, plot = False):
-    
+
+def objective_function(optimization_data, plot=False):
+
     number_of_neutralizers = len(optimization_data.neutralizers)
     number_of_modes = len(optimization_data.primary_system_natural_frequencies)
     frequency_discretization = len(optimization_data.frequencies)
+    phi = np.asarray(optimization_data.primary_system_modes)
     composed_system_stiffness = np.zeros((number_of_modes, number_of_modes), dtype=complex)
     receptance = np.zeros(frequency_discretization, dtype=complex)
-    
-    complex_shear_module_per_neutralizer, frequency_index_per_neutralizer, loss_factor_per_neutralizer = calculate_viscoelastic_properties(optimization_data, number_of_neutralizers)
+
+    (complex_shear_module_per_neutralizer, frequency_index_per_neutralizer, loss_factor_per_neutralizer) = calculate_viscoelastic_properties(optimization_data, number_of_neutralizers)
 
     for i in range(frequency_discretization):
         modal_mass_array = np.zeros((number_of_modes, number_of_modes), dtype=complex)
         modal_damp_array = np.zeros((number_of_modes, number_of_modes), dtype=complex)
+
         for j in range(number_of_modes):
             for k in range(number_of_modes):
                 for l in range(number_of_neutralizers):
-                    frenquency_ratio = optimization_data.frequencies[i]/optimization_data.neutralizers[l].frequency
+                    frequency_ratio = optimization_data.frequencies[i] / optimization_data.neutralizers[l].frequency
+
                     equivalent_mass, equivalent_damp = equivalent_parameters(
                         optimization_data.neutralizers[l],
-                        frenquency_ratio,
+                        frequency_ratio,
                         calculate_real_shear_module_ratio(complex_shear_module_per_neutralizer, l, i, frequency_index_per_neutralizer),
                         calculate_loss_factor_per_neutralizer(loss_factor_per_neutralizer, l, i),
                         calculate_dynamic_stiffness(optimization_data.user_defined_dynamic_stiffnesses, optimization_data.neutralizers[l].dynamic_stiffness, i),
-                        calculate_real_shear_module_at_system_frequency(complex_shear_module_per_neutralizer, l, i, frequency_index_per_neutralizer)
+                        calculate_real_shear_module_at_system_frequency(complex_shear_module_per_neutralizer, l, i, frequency_index_per_neutralizer),
                     )
 
                     if optimization_data.neutralizers[l].type in [0, 1, 2, 3]:
-                        modal_mass_array[j][k] += equivalent_mass * optimization_data.primary_system_modes[j][optimization_data.neutralizers[l].modal_position] * optimization_data.primary_system_modes[k][optimization_data.neutralizers[l].modal_position]
-                        modal_damp_array[j][k] += equivalent_damp * optimization_data.primary_system_modes[j][optimization_data.neutralizers[l].modal_position] * optimization_data.primary_system_modes[k][optimization_data.neutralizers[l].modal_position]
-            
+                        node = optimization_data.neutralizers[l].modal_position
+                        modal_mass_array[j, k] += equivalent_mass * phi[j, node] * phi[k, node]
+                        modal_damp_array[j, k] += equivalent_damp * phi[j, node] * phi[k, node]
+
                     if optimization_data.neutralizers[l].type == 4:
-                        modal_mass_array[j][k] += equivalent_mass * optimization_data.primary_system_modes[j][optimization_data.neutralizers[l].modal_position_tip] * optimization_data.primary_system_modes[k][optimization_data.neutralizers[l].modal_position_tip]
-                        modal_mass_array[j][k] -= equivalent_mass * optimization_data.primary_system_modes[j][optimization_data.neutralizers[l].modal_position] * optimization_data.primary_system_modes[k][optimization_data.neutralizers[l].modal_position_tip]
-                        modal_mass_array[j][k] -= equivalent_mass * optimization_data.primary_system_modes[j][optimization_data.neutralizers[l].modal_position_tip] * optimization_data.primary_system_modes[k][optimization_data.neutralizers[l].modal_position]
+                        node = optimization_data.neutralizers[l].modal_position
+                        tip = optimization_data.neutralizers[l].modal_position_tip
+                        modal_mass_array[j, k] += equivalent_mass * (phi[j, tip] * phi[k, tip] - phi[j, node] * phi[k, tip] - phi[j, tip] * phi[k, node])
+                        modal_damp_array[j, k] += equivalent_damp * (phi[j, tip] * phi[k, tip] - phi[j, node] * phi[k, tip] - phi[j, tip] * phi[k, node])
 
-                        modal_damp_array[j][k] += equivalent_damp * optimization_data.primary_system_modes[j][optimization_data.neutralizers[l].modal_position_tip] * optimization_data.primary_system_modes[k][optimization_data.neutralizers[l].modal_position_tip]
-                        modal_damp_array[j][k] -= equivalent_damp * optimization_data.primary_system_modes[j][optimization_data.neutralizers[l].modal_position] * optimization_data.primary_system_modes[k][optimization_data.neutralizers[l].modal_position_tip]
-                        modal_damp_array[j][k] -= equivalent_damp * optimization_data.primary_system_modes[j][optimization_data.neutralizers[l].modal_position_tip] * optimization_data.primary_system_modes[k][optimization_data.neutralizers[l].modal_position]     
         for j in range(number_of_modes):
-            primary_system_stiffness = complex(optimization_data.primary_system_natural_frequencies[j]**2 - optimization_data.frequencies[i]**2, optimization_data.primary_system_natural_frequencies[j]**2 * optimization_data.primary_system_modal_damping[j])
+            primary_system_stiffness = complex(optimization_data.primary_system_natural_frequencies[j] ** 2 - optimization_data.frequencies[i] ** 2, optimization_data.primary_system_natural_frequencies[j] ** 2 * optimization_data.primary_system_modal_damping[j])
+
             for k in range(number_of_modes):
-                modal_stiffness = complex(-optimization_data.frequencies[i]**2 * modal_mass_array[j][k],optimization_data.frequencies[i] * modal_damp_array[j][k]) 
-                composed_system_stiffness[j][k] =  modal_stiffness
-                if (j==k):
-                    composed_system_stiffness[j][k] = modal_stiffness + primary_system_stiffness
+                modal_stiffness = complex(-(optimization_data.frequencies[i] ** 2) * modal_mass_array[j, k], optimization_data.frequencies[i] * modal_damp_array[j, k])
+                composed_system_stiffness[j, k] = modal_stiffness
+                if j == k:
+                    composed_system_stiffness[j, k] += primary_system_stiffness
+
         inverse_composed_system_matrix = np.linalg.inv(composed_system_stiffness)
-        for j in range(number_of_modes):
-            for k in range(number_of_modes):
-                receptance[i] += inverse_composed_system_matrix [j][k] * optimization_data.primary_system_modes[j][optimization_data.response_node_optimization] * optimization_data.primary_system_modes[k][optimization_data.excitation_node_optimization]
+        H = phi.T @ inverse_composed_system_matrix @ phi
+        receptance[i] = H[optimization_data.response_node_optimization, optimization_data.excitation_node_optimization]
 
-    """# Plotting the data
-    if(plot):
-        plt.plot(optimization_data.frequencies/(2*np.pi),20*np.log10(abs(receptance)))
-        plt.show()
-    """
     return receptance
 
-def equivalent_parameters(neutralizer, frequency_ratio, real_shear_module_ratio = 0, loss_factor = 0, dynamic_stifness = 0.0, real_shear_module_at_system_frequency = 0.0):
+
+def equivalent_parameters(neutralizer, frequency_ratio, real_shear_module_ratio=0, loss_factor=0, dynamic_stifness=0.0, real_shear_module_at_system_frequency=0.0):
     equivalent_mass = 0
     equivalent_damp = 0
-    if(neutralizer.type == 0):
+    if neutralizer.type == 0:
         frequency = frequency_ratio * neutralizer.frequency
-        equivalent_damp = complex(0, dynamic_stifness/frequency) 
-        equivalent_mass = complex(dynamic_stifness/frequency**2,0)
-    if(neutralizer.type == 1):
-        denominator = (frequency_ratio ** 2 - real_shear_module_ratio) ** 2 + (real_shear_module_ratio * loss_factor) ** 2
-        equivalent_damp = neutralizer.mass * neutralizer.frequency * real_shear_module_ratio * loss_factor * frequency_ratio ** 3 / denominator
-        equivalent_mass = - neutralizer.mass * real_shear_module_ratio * (frequency_ratio ** 2 - real_shear_module_ratio * (1 + loss_factor ** 2)) / denominator
-    if(neutralizer.type == 2):
-        denominator = (frequency_ratio ** 2. - 1.) ** 2. + (2. * neutralizer.damp * frequency_ratio) ** 2.
-        equivalent_damp = neutralizer.mass * neutralizer.frequency * 2. * neutralizer.damp * frequency_ratio ** 4. / denominator
-        equivalent_mass = -neutralizer.mass * (frequency_ratio ** 2. - (1. + (2. * neutralizer.damp * frequency_ratio) ** 2.)) / denominator
-    if(neutralizer.type in (3, 4)):
+        equivalent_damp = complex(0, dynamic_stifness / frequency)
+        equivalent_mass = complex(dynamic_stifness / frequency**2, 0)
+    if neutralizer.type == 1:
+        denominator = (frequency_ratio**2 - real_shear_module_ratio) ** 2 + (real_shear_module_ratio * loss_factor) ** 2
+        equivalent_damp = neutralizer.mass * neutralizer.frequency * real_shear_module_ratio * loss_factor * frequency_ratio**3 / denominator
+        equivalent_mass = -neutralizer.mass * real_shear_module_ratio * (frequency_ratio**2 - real_shear_module_ratio * (1 + loss_factor**2)) / denominator
+    if neutralizer.type == 2:
+        denominator = (frequency_ratio**2.0 - 1.0) ** 2.0 + (2.0 * neutralizer.damp * frequency_ratio) ** 2.0
+        equivalent_damp = neutralizer.mass * neutralizer.frequency * 2.0 * neutralizer.damp * frequency_ratio**4.0 / denominator
+        equivalent_mass = -neutralizer.mass * (frequency_ratio**2.0 - (1.0 + (2.0 * neutralizer.damp * frequency_ratio) ** 2.0)) / denominator
+    if neutralizer.type in (3, 4):
         denominator = frequency_ratio * neutralizer.frequency
         equivalent_mass = -neutralizer.shape_factor * real_shear_module_at_system_frequency / denominator**2
         equivalent_damp = neutralizer.shape_factor * real_shear_module_at_system_frequency * loss_factor / denominator
 
     return equivalent_mass, equivalent_damp
+
 
 def calculate_viscoelastic_properties(optimization_data, number_of_neutralizers):
 
@@ -90,28 +91,32 @@ def calculate_viscoelastic_properties(optimization_data, number_of_neutralizers)
             complex_shear_module_per_neutralizer.append(optimization_data.complex_shear_moduluses[optimization_data.neutralizers[l].viscoelastic_material])
             frequency_index_per_neutralizer.append(np.abs(optimization_data.frequencies - optimization_data.neutralizers[l].frequency).argmin())
             loss_factor_per_neutralizer.append(optimization_data.complex_shear_moduluses[optimization_data.neutralizers[l].viscoelastic_material].imag / optimization_data.complex_shear_moduluses[optimization_data.neutralizers[l].viscoelastic_material].real)
-        
-    return complex_shear_module_per_neutralizer, frequency_index_per_neutralizer, loss_factor_per_neutralizer
 
-def calculate_real_shear_module_ratio(complex_shear_module_per_neutralizer,neutralizers_index,frequency_index,frequency_index_per_neutralizer):
+    return (complex_shear_module_per_neutralizer, frequency_index_per_neutralizer, loss_factor_per_neutralizer)
+
+
+def calculate_real_shear_module_ratio(complex_shear_module_per_neutralizer, neutralizers_index, frequency_index, frequency_index_per_neutralizer):
     real_shear_module_ratio = 0
     if len(complex_shear_module_per_neutralizer) > 0:
         real_shear_module_ratio = complex_shear_module_per_neutralizer[neutralizers_index][frequency_index].real / complex_shear_module_per_neutralizer[neutralizers_index][frequency_index_per_neutralizer[neutralizers_index]].real
     return real_shear_module_ratio
 
-def calculate_loss_factor_per_neutralizer(loss_factor_per_neutralizer,neutralizers_index,frequency_index):
+
+def calculate_loss_factor_per_neutralizer(loss_factor_per_neutralizer, neutralizers_index, frequency_index):
     loss_factor = 0
     if len(loss_factor_per_neutralizer) > 0:
         loss_factor = loss_factor_per_neutralizer[neutralizers_index][frequency_index]
     return loss_factor
 
-def calculate_dynamic_stiffness(user_defined_dynamic_stiffnesses,dynamic_stiffness_index,frequency_index):
+
+def calculate_dynamic_stiffness(user_defined_dynamic_stiffnesses, dynamic_stiffness_index, frequency_index):
     dynamic_stiffness = 0
     if len(user_defined_dynamic_stiffnesses) > 0:
         dynamic_stiffness = user_defined_dynamic_stiffnesses[dynamic_stiffness_index][frequency_index]
     return dynamic_stiffness
 
-def calculate_real_shear_module_at_system_frequency(complex_shear_module_per_neutralizer,neutralizers_index,frequency_index,frequency_index_per_neutralizer):
+
+def calculate_real_shear_module_at_system_frequency(complex_shear_module_per_neutralizer, neutralizers_index, frequency_index, frequency_index_per_neutralizer):
     real_shear_module_at_system_frequency = 0
     if len(complex_shear_module_per_neutralizer) > 0:
         real_shear_module_at_system_frequency = complex_shear_module_per_neutralizer[neutralizers_index][frequency_index].real
--- services/optimization_preparation_service.py
+++ services/optimization_preparation_service.py
@@ -1,25 +1,22 @@
 import numpy as np
-from models import ObjectiveFunctionInput,NeutralizerParameters
+from models import ObjectiveFunctionInput, NeutralizerParameters
 from typing import List
 import pygad
 from services.objective_function_service import objective_function
 from services.primary_system_service import primary_system_response
 
-def prepare_objective_function_input(optimization_data, plot = False):
+
+def prepare_objective_function_input(optimization_data, plot=False):
 
     frequencies = 0
-    if(plot):
-        frequencies = np.linspace(optimization_data.plot_lower_bound, optimization_data.plot_upper_bound, optimization_data.plot_discretization)    
+    if plot:
+        frequencies = np.linspace(optimization_data.plot_lower_bound, optimization_data.plot_upper_bound, optimization_data.plot_discretization)
     else:
         frequencies = np.linspace(optimization_data.objective_function_search_lower_bound, optimization_data.objective_function_search_upper_bound, optimization_data.objective_function_search_discretization)
 
     neutralizers: List[NeutralizerParameters] = []
     for neutralizer in optimization_data.neutralizers:
-        neutralizer_parameters = NeutralizerParameters(
-            mass = neutralizer.mass,
-            original_mass = neutralizer.mass,
-            mass_type_user_defined = neutralizer.mass_type_user_defined
-        )
+        neutralizer_parameters = NeutralizerParameters(mass=neutralizer.mass, original_mass=neutralizer.mass, mass_type_user_defined=neutralizer.mass_type_user_defined)
         neutralizers.append(neutralizer_parameters)
 
     complex_shear_moduluses = []
@@ -33,7 +30,7 @@ def prepare_objective_function_input(optimization_data, plot = False):
 
     excitation_node = 0
     response_node = 0
-    if(plot):
+    if plot:
         excitation_node = optimization_data.excitation_node_plot
         response_node = optimization_data.response_node_plot
     else:
@@ -41,20 +38,21 @@ def prepare_objective_function_input(optimization_data, plot = False):
         response_node = optimization_data.response_node_optimization
 
     objective_function_input = ObjectiveFunctionInput(
-        frequencies = frequencies,
-        complex_shear_moduluses = complex_shear_moduluses,
-        user_defined_dynamic_stiffnesses = user_defined_dynamic_stiffnesses,
-        neutralizers = neutralizers,
-        primary_system_natural_frequencies = optimization_data.primary_system_natural_frequencies,
-        primary_system_modal_damping = optimization_data.primary_system_modal_damping,
-        primary_system_modes = optimization_data.primary_system_modes,
-        excitation_node_optimization = excitation_node,
-        response_node_optimization = response_node,
+        frequencies=frequencies,
+        complex_shear_moduluses=complex_shear_moduluses,
+        user_defined_dynamic_stiffnesses=user_defined_dynamic_stiffnesses,
+        neutralizers=neutralizers,
+        primary_system_natural_frequencies=optimization_data.primary_system_natural_frequencies,
+        primary_system_modal_damping=optimization_data.primary_system_modal_damping,
+        primary_system_modes=optimization_data.primary_system_modes,
+        excitation_node_optimization=excitation_node,
+        response_node_optimization=response_node,
         objective_function_search_lower_bound=optimization_data.objective_function_search_lower_bound,
-        objective_function_search_upper_bound=optimization_data.objective_function_search_upper_bound
+        objective_function_search_upper_bound=optimization_data.objective_function_search_upper_bound,
     )
 
     return objective_function_input
+
 
 def complex_shear_modulus(viscoelastic_material, frequencies):
 
@@ -62,27 +60,26 @@ def complex_shear_modulus(viscoelastic_material, frequencies):
     upperShearModulus = viscoelastic_material.upperShearModulus
     temperatureShiftingFactor = viscoelastic_material.temperatureShiftingFactor
     alfaT = alfa(viscoelastic_material.referenceTemperature, viscoelastic_material.workingTemperature, viscoelastic_material.teta1, viscoelastic_material.teta2)
-    
+
     numerator = lowerShearModulus + (upperShearModulus * temperatureShiftingFactor) * (1j * alfaT * frequencies) ** viscoelastic_material.fractionalDerivativeParameter
     denominator = 1.0 + temperatureShiftingFactor * (1j * alfaT * frequencies) ** viscoelastic_material.fractionalDerivativeParameter
-    
+
     complex_shear_modulus = numerator / denominator
 
     return complex_shear_modulus
+
 
 def alfa(referenceTemperature, workingTemperature, teta1, teta2):
     deltaT = workingTemperature - referenceTemperature
     alfa = 10.0 ** (-teta1 * deltaT / (teta2 + deltaT))
     return alfa
 
+
 def compute_neutralizer_mass(neutralizer, optimization_data):
     if neutralizer.mass_type_user_defined:
         return neutralizer.original_mass
 
-    valid_modes = [
-        i for i, freq in enumerate(optimization_data.primary_system_natural_frequencies)
-        if optimization_data.objective_function_search_lower_bound <= freq <= optimization_data.objective_function_search_upper_bound
-    ]
+    valid_modes = [i for i, freq in enumerate(optimization_data.primary_system_natural_frequencies) if optimization_data.objective_function_search_lower_bound <= freq <= optimization_data.objective_function_search_upper_bound]
 
     total_mass = 0.0
     for i in valid_modes:
@@ -92,6 +89,7 @@ def compute_neutralizer_mass(neutralizer, optimization_data):
 
     print(f"total_mass: {total_mass}")
     return total_mass / len(valid_modes) if valid_modes else 0.0
+
 
 def insert_neutralizers(objective_function_input, ga_variables_values, ga_variables_names, ga_variables_per_neutralizer):
     var_offset = 0
@@ -114,15 +112,14 @@ def insert_neutralizers(objective_function_input, ga_variables_values, ga_variab
 
     return objective_function_input
 
+
 def calculate_receptances_with_temperature_detuning(optimization_input, plot_input_with_neutralizers):
     receptances_with_detuning = []
-    
+
     # Iterate through all detuning temperatures
     for temperature in optimization_input.additional_parameters.temperature_detuning:
         # Work on a fresh copy of the input
-        modified_input = ObjectiveFunctionInput(
-            **vars(plot_input_with_neutralizers)
-        )
+        modified_input = ObjectiveFunctionInput(**vars(plot_input_with_neutralizers))
 
         # For each neutralizer that has a viscoelastic material,
         # recompute its complex shear modulus with the detuned temperature
@@ -145,13 +142,10 @@ def calculate_receptances_with_temperature_detuning(optimization_input, plot_inp
         modified_input.complex_shear_moduluses = complex_shear_mods
 
         # Compute receptance
-        receptance = objective_function(modified_input, True)
+        receptance, objective = objective_function(modified_input, True)
 
         # Store result as { "temperature": <val>, "receptance": [...] }
-        receptances_with_detuning.append({
-            "temperature": temperature,
-            "receptance": receptance
-        })
+        receptances_with_detuning.append({"temperature": temperature, "receptance": receptance})
 
     return receptances_with_detuning
 
@@ -162,15 +156,16 @@ def optimal_solution(optimization_input, solution, gene_name, gene_per_neutraliz
     plot_input_with_neutralizers = insert_neutralizers(plot_input, solution, gene_name, gene_per_neutralizer)
 
     # Calculate standard receptances
-    composed_system_receptance = objective_function(plot_input_with_neutralizers, True)
+    composed_system_receptance, objective = objective_function(plot_input_with_neutralizers, True)
     primary_system_receptance = primary_system_response(plot_input_with_neutralizers)
 
     # Calculate receptances under temperature detuning (only if non-empty)
     receptances_with_detuning = []
-    if (optimization_input.additional_parameters.temperature_detuning is not None and len(optimization_input.additional_parameters.temperature_detuning) > 0):
+    if optimization_input.additional_parameters.temperature_detuning is not None and len(optimization_input.additional_parameters.temperature_detuning) > 0:
         receptances_with_detuning = calculate_receptances_with_temperature_detuning(optimization_input, plot_input_with_neutralizers)
-    
+
     return composed_system_receptance, primary_system_receptance, receptances_with_detuning, plot_input_with_neutralizers.neutralizers
+
 
 def ga_preparation(optimization_input):
     objective_function_input = prepare_objective_function_input(optimization_input)
@@ -194,7 +189,6 @@ def ga_preparation(optimization_input):
             gene_name.append(real_variable.name)
             gene_per_neutralizer_count += 1
 
-        
         # Handle integer variables
         for integer_variable in neutralizer.optimization_variables.integer:
             gene_types.append(int)
@@ -204,14 +198,13 @@ def ga_preparation(optimization_input):
 
         gene_per_neutralizer.append(gene_per_neutralizer_count)
 
-
     def objective_funtion_wrapper_factory(objective_function_input, gene_name, gene_per_neutralizer):
 
         def objective_funtion_wrapper(ga_instance, solution, solution_idx):
-            objective_function_input_with_neutralizers = insert_neutralizers(objective_function_input,solution, gene_name, gene_per_neutralizer)
-            receptance = objective_function(objective_function_input_with_neutralizers)
-            return -np.sum(receptance * np.conj(receptance)).real
-        
+            objective_function_input_with_neutralizers = insert_neutralizers(objective_function_input, solution, gene_name, gene_per_neutralizer)
+            receptance, objective = objective_function(objective_function_input_with_neutralizers)
+            return -objective
+
         return objective_funtion_wrapper
 
     # Define GA parameters from the genetic_algorithm object
@@ -222,14 +215,6 @@ def ga_preparation(optimization_input):
     mutation_probability = optimization_input.genetic_algorithm.mutation / 100
 
     # Create an instance of the GA
-    ga_instance = pygad.GA(num_generations=num_generations,
-                        num_parents_mating=num_parents_mating,
-                        fitness_func=objective_funtion_wrapper_factory(objective_function_input, gene_name, gene_per_neutralizer),
-                        sol_per_pop=sol_per_pop,
-                        num_genes=len(gene_types),
-                        gene_type=gene_types,
-                        gene_space=gene_space,
-                        crossover_probability=crossover_probability,
-                        mutation_probability=mutation_probability)
+    ga_instance = pygad.GA(num_generations=num_generations, num_parents_mating=num_parents_mating, fitness_func=objective_funtion_wrapper_factory(objective_function_input, gene_name, gene_per_neutralizer), sol_per_pop=sol_per_pop, num_genes=len(gene_types), gene_type=gene_types, gene_space=gene_space, crossover_probability=crossover_probability, mutation_probability=mutation_probability)
 
-    return ga_instance,gene_name,gene_per_neutralizer
+    return ga_instance, gene_name, gene_per_neutralizer
